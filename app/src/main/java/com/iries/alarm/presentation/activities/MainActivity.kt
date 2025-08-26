88610746d3738120543d0934fa3c9236b578e82c
Victoria-Developer <77235839+Victoria-Developer@users.noreply.github.com>
2025-08-26 13:45:59 +0800

    - Refactored MainActivity to handle auth api callback.

--- app/src/main/java/com/iries/alarm/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/iries/alarm/presentation/activities/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.iries.alarm.presentation.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.iries.alarm.presentation.navigation.AppNavigation
@@ -13,19 +17,28 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
+
+        mainViewModel.handleIntent(intent)
         setContent {
             IriesAlarmTheme {
+                val loginCode by mainViewModel.loginCode.collectAsState()
                 AppNavigation(
-                    navController = rememberNavController()
+                    navController = rememberNavController(),
+                    loginCode = loginCode
                 )
             }
         }
+    }
 
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        mainViewModel.handleIntent(intent)
     }
 }
--- app/src/main/java/com/iries/alarm/presentation/screens/auth/AuthScreen.kt
+++ app/src/main/java/com/iries/alarm/presentation/screens/auth/AuthScreen.kt
@@ -1,9 +1,5 @@
 package com.iries.alarm.presentation.screens.auth
 
-import android.app.Activity
-import android.content.Intent
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +9,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -20,20 +17,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.iries.alarm.domain.AuthState
-import com.iries.alarm.presentation.activities.AuthActivity
 
 @Composable
-fun AuthScreen(authViewModel: AuthViewModel) {
-    val context = LocalContext.current
-    val state by authViewModel.state.collectAsState()
+fun AuthScreen(
+    loginCode: String?,
+    onRedirectToMusicScreen: () -> Unit,
+    onRedirectToAlarmsScreen: () -> Unit
+) {
 
-    val loginLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val code = result.data?.getStringExtra("code")
-            code?.let { authViewModel.authorizeUser(it) }
+    val context = LocalContext.current
+    val viewModel: AuthViewModel = hiltViewModel()
+    val state by viewModel.state.collectAsState()
+
+    LaunchedEffect(loginCode) {
+        if (!loginCode.isNullOrEmpty() && state == AuthState.RequiresLogin) {
+            viewModel.authorizeUser(loginCode)
+        }
+    }
+
+    LaunchedEffect(state) {
+        when (state) {
+            AuthState.AuthorizedFirstTime -> onRedirectToMusicScreen()
+            AuthState.Authorized -> onRedirectToAlarmsScreen()
+            else -> Unit
         }
     }
 
@@ -49,19 +57,18 @@ fun AuthScreen(authViewModel: AuthViewModel) {
                 Spacer(Modifier.padding(vertical = 15.dp))
                 CircularProgressIndicator()
             }
+
             AuthState.RequiresLogin -> {
                 Text(
                     "Please, login to your Sound Cloud account to continue.",
                     textAlign = TextAlign.Center
                 )
                 Spacer(Modifier.padding(vertical = 15.dp))
-                Button(onClick = {
-                    val intent = Intent(context, AuthActivity::class.java)
-                    loginLauncher.launch(intent)
-                }) {
+                Button(onClick = { viewModel.login(context) }) {
                     Text("Login")
                 }
             }
+
             AuthState.Authorized,
             AuthState.AuthorizedFirstTime -> {
                 Text("You were successfully authorized!.")
